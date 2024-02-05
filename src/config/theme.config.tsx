56f33757e733b39b85bf8a7b3acde22cf2faaf18
NahuelRosso <nahuelgrosso25@gmail.com>
2024-02-05 12:54:24 -0300

    Provider, context con Alert

--- src/config/theme.config.tsx
+++ src/config/theme.config.tsx
@@ -7,7 +7,10 @@ type ThemeProp = {
 export enum themePalette {
   BG = "#12181b",
   LIME = "#C8FA5F",
-  FONT_GLOBAL = "'JetBrains Mono', monospace"
+  FONT_GLOBAL = "'JetBrains Mono', monospace",
+  //Aert Style
+  ERROR_MAIN="f44336",
+  BG_ERROR_MAIN="rgba(244,67,54,0.1)"
 }
 const theme = createTheme({
   palette: {
@@ -31,8 +34,22 @@ const theme = createTheme({
                     borderRadius:"0.5rem"
                 }
             }
-        }
-    }
+        },
+        MuiAlert:{
+          defaultProps:{
+            style:{
+              borderRadius:"0.8em",
+              fontSize:"1em",
+            },
+          },
+          styleOverrides:{
+            standardError:{
+              border:`1px solid ${themePalette.ERROR_MAIN}`,
+              background:themePalette.BG_ERROR_MAIN,
+            }
+          }
+        },
+    },
 });
 export const ThemeConfig: React.FC<ThemeProp> = ({ children }) => {
   return (
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,15 @@
 import { Container, Button } from "@mui/material";
 import React from "react";
+import { useNotification } from "../../context/notification.context";
 
 export const HomePage: React.FC<{}>=()=>{
+  const {getError}= useNotification()
+  const handleClick=()=>{
+    getError("Hola Mundo")
+  }
     return(
         <Container sx={{mt:9}} maxWidth="xl">
-        <Button fullWidth variant="contained">
+        <Button onClick={handleClick} fullWidth variant="contained">
          Home
         </Button>
       </Container>
